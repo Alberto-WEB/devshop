2ee998048952c36c46be36502416db8e45a5be62
Carlos Guzman <alberto.guzman.220793@gmail.com>
2022-03-27 21:47:27 -0600

    add front

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,17 +1,21 @@
 <template>
-    {{ products }}
+    <app-layout>
+
+    </app-layout>
 </template>
 
 
 <script>
     import { defineComponent } from 'vue'
     import { Head, Link } from '@inertiajs/inertia-vue3';
+    import AppLayout from '@Layouts/AppLayout'
 
     export default defineComponent({
         props: ['products'],
         components: {
             Head,
             Link,
+            AppLayout,
         },
     })
 </script>
